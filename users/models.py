8e91368491311f549e864a414688548a0f3d806e
Your Name <your.email@example.com>
2026-02-09 08:54:49 +0530

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField( upload_to='profile_pics',
+      default='default.jpg',
+
+        blank=True,
+       )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image and self.image.name != 'default.jpg':
+            try:
+                img = Image.open(self.image.path)
+
+                if img.height > 300 or img.width > 300:
+                    img.thumbnail((300, 300))
+                    img.save(self.image.path)
+            except Exception:
+                pass
