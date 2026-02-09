cb1de9368d6911c0aae1e6630077a2f5224af719
Your Name <your.email@example.com>
2026-02-09 09:32:26 +0530

    Fixed staticfiles issue

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # your source static files (like static/image/)
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # folder where collectstatic gathers files
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
