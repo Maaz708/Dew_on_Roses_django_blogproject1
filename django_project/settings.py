36dc3b099d95c8e9daec6d3d53c26d25469ff4d7
Your Name <your.email@example.com>
2026-02-09 10:35:59 +0530

    Fix database config for local and production

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from dotenv import load_dotenv
+import dj_database_url
+
 from pathlib import Path
 import os
 
@@ -87,10 +89,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 
